3f165306f8b20bd83c92086172828d87b4c8796c
Petteri Havia <petsku15@hotmail.fi>
2023-04-16 16:39:08 +0300

    useEffect testing

--- Chapter1New/react-tutorial/src/App.js
+++ Chapter1New/react-tutorial/src/App.js
@@ -1,15 +1,19 @@
-import React from "react";
+import React, { useEffect } from "react";
 import TweetList from './components/TweetList';
 import CreateTweet from './components/CreateTweet';
 import { useState } from "react";
 
 function App() {
   //Normal javaScript here like functions and const
-
   //State
   const [textInput, setTextInput] = useState("");
   const [tweets, setTweets] = useState([]);
   const [name, setName] = useState("PH");
+
+  useEffect(()=> {
+    console.log('we run a function');
+  },[]);
+
 
   return (
     <div className="App">
