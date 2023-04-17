98abfa154a3f5bfc297c05100f4c25aa9878f692
Petteri Havia <petsku15@hotmail.fi>
2023-04-17 18:04:54 +0300

    Navigation with library control

---
+++ Chapter3/react-player/src/components/Navigation.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faMusic } from '@fortawesome/free-solid-svg-icons';
+
+const Navigation = ({setLibraryStatus, libraryStatus}) => {
+
+    //Set state false/true to show library
+    const checkLibraryStatus = () => { 
+        setLibraryStatus(!libraryStatus)
+    };
+
+    return(
+        <nav>
+            <h1>Waves</h1>
+            <button onClick={checkLibraryStatus}>Library<FontAwesomeIcon icon={faMusic}/></button>
+        </nav>
+    );
+}
+
+export default Navigation;
