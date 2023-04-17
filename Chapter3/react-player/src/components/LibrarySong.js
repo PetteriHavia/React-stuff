1177d7e8dc2ed92a941767eb6b749694c7a072c1
Petteri Havia <petsku15@hotmail.fi>
2023-04-17 18:07:15 +0300

    Select song from library

--- Chapter3/react-player/src/components/LibrarySong.js
+++ Chapter3/react-player/src/components/LibrarySong.js
@@ -1,15 +1,16 @@
 import React from 'react';
 
-const LibrarySong = ({song,  songs, setCurrentSong, id}) => {
+const LibrarySong = ({song,  songs, currentSong, setCurrentSong, id}) => {
 
 
+    //Select clicked song from library
     const songSelectHandler = () => {
         //const selectedSong = songs.filter((individual) => individual.id === song.id);
         setCurrentSong(song);
     };
 
     return(
-        <div onClick={songSelectHandler} className="library-song">
+        <div onClick={songSelectHandler} className={`library-song ${song.id === currentSong.id ? "selected" : ""} `}>
             <img src={song.cover} alt={song.name}/>
             <div className="song-description">
             <h3>{song.name}</h3>
