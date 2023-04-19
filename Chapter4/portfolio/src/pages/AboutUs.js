7e1ca810616ff77f9918938874202da64c3d35e5
Petteri Havia <petsku15@hotmail.fi>
2023-04-19 21:25:11 +0300

    Animation effects

--- Chapter4/portfolio/src/pages/AboutUs.js
+++ Chapter4/portfolio/src/pages/AboutUs.js
@@ -3,14 +3,17 @@ import React from "react";
 import About from "../components/About";
 import Services from "../components/Services";
 import Faq from "../components/Faq";
+//Animations
+import { motion } from 'framer-motion';
+import { pageAnimation } from '../animation';
 
 const AboutUs = () => {
   return (
-    <>
+    <motion.div variants={pageAnimation} initial='hidden' animate='show' exit='exit'>
       <About />
       <Services />
       <Faq />
-    </>
+    </motion.div>
   );
 };
 
