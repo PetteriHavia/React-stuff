c7e674203169cc8685b7724198f8feeebc70dc17
Petteri Havia <petsku15@hotmail.fi>
2023-04-26 22:34:02 +0300

    new action for searching games

--- Chapter6/gamebase/src/reducers/gamesReducer.js
+++ Chapter6/gamebase/src/reducers/gamesReducer.js
@@ -19,7 +19,12 @@ const gamesReducer = (state=initState, action) => {
             return {
                 ...state,
                 searched: action.payload.searched,
-            }    
+            }
+        case "CLEAR_SEARCH":
+            return {
+                ...state,
+                searched: [],
+            }        
         default:
             return {...state}
     }
