647d691c0695351012ef7abf4bebaa32b443d2ec
Petteri Havia <petsku15@hotmail.fi>
2023-04-26 22:32:37 +0300

    Animation fix

--- Chapter6/gamebase/src/pages/home.js
+++ Chapter6/gamebase/src/pages/home.js
@@ -9,6 +9,7 @@ import GameDetail from "../components/GameDetail";
 import styled from "styled-components";
 import { motion, AnimatePresence} from "framer-motion";
 import { useLocation } from "react-router-dom";
+import { fadeIn } from "../animations";
 
 const Home = () => {
   //Get current location
@@ -23,13 +24,24 @@ const Home = () => {
   }, [dispatch]);
 
   //Get data from state
-  const { popular, newGames, upComing } = useSelector((state) => state.games);
+  const { popular, newGames, upComing, searched } = useSelector((state) => state.games);
 
   return (
-    <GameList>
+    <GameList variants={fadeIn} initial='hidden' animate='show'>
       <AnimatePresence type="crossfade">
         {pathId && <GameDetail pathId={pathId}/>}
       </AnimatePresence>
+      {searched.length ?  (
+        <div className="Searched">
+          <h2>Search Results</h2>
+          <Games>
+            {searched.map((game) => (
+              <Game game={game} key={game.id} />
+            ))}
+          </Games>
+        </div>
+      ) : ("")
+      }
       <h2>Upcoming Games</h2>
       <Games>
         {upComing.map((game) => (
@@ -57,17 +69,25 @@ const GameList = styled(motion.div)`
   h2 {
     padding: 5rem 0rem;
   }
+  @media (max-width: 768px) {
+    padding: 0rem 2rem;
+  }
 `;
 
 const Games = styled(motion.div)`
   min-height: 80vh;
   display: grid;
-  grid-template-columns: repeat(
-    auto-fit,
-    minmax(450px, 1fr)
-  ); //repeat 350 minimun space for column, if not enough space then take rest of the space with 1fr
+  grid-template-columns: repeat( auto-fit, minmax(450px, 1fr)); //repeat 350 minimun space for column, if not enough space then take rest of the space with 1fr
   grid-column-gap: 3rem;
   grid-row-gap: 5rem;
+
+  @media (max-width: 1200px) {
+    grid-template-columns: repeat( auto-fit, minmax(250px, 1fr));
+
+    h3{
+      font-size:1rem;
+    }
+  }
 `;
 
 export default Home;
